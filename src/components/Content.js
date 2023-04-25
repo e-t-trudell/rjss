e004a672e4171c79fcf3d38a27cf2f7fe8fbb1f1
Eric <ettrudell17@gmail.com>
2023-04-24 19:39:37 -0600

    added java backend with config to filter cors, auth content, light styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import AuthContent from './AuthContent';
 import WelcomeContent from './WelcomeContent';
 
 export default class Content extends React.Component {
     render(){
         return (
             <>
-            <WelcomeContent/>
+                <WelcomeContent/>
+                <AuthContent/>
             </>
         );
     }
